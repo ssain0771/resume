e06e8b6d7d7d9f6d233140bbaf428ced1c272c1a
ssain0771@mtroyal.ca <ssain0771@mtroyal.ca>
2026-02-20 15:41:39 -0700

    new type

--- scripts/resumeScripts.js
+++ scripts/resumeScripts.js
@@ -223,6 +223,7 @@
         const labelMap = {
             full: "Full",
             analyst: "Data Analyst",
+            developer: "Developer",
             geospatial: "Geospatial",
             science: "Science",
         };
